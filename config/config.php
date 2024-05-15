59209c440ffaa6b5666c8158f2e988d9f811b2fc
NguyenTHiNgocHien <125424973+NguyenTHiNgocHien@users.noreply.github.com>
2024-05-15 08:56:21 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define("DB_HOST", 'localhost');
-define("DB_USER", 'root');
-define("DB_PASS", '');
+define("DB_HOST", 'csdlban.mysql.database.azure.com');
+define("DB_USER", 'ban');
+define("DB_PASS", '1123456miT');
 define("DB_NAME", 'instrumentstore');
 ?>
